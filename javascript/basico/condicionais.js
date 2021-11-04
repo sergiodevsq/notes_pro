5d10e5c15aa19f3542b34a123d2e01ee7678257b
sergiodevsq <sergio@sq.dev.br>
2021-11-04 16:19:42 -0300

    Atualização condicionais

--- javascript/basico/condicionais.js
+++ javascript/basico/condicionais.js
@@ -13,6 +13,27 @@
     else
         instrução2
 
-    
+    switch(n) {
+        case 1:
+        // Começa aqui se n === 1
+        // Executa o bloco de código #1.
+        break;
+        // Para aqui
+        case 2:
+        // Começa aqui se n === 2
+        // Executa o bloco de código #2.
+        break;
+        // Para aqui
+        case 3:
+        // Começa aqui se n === 3
+        // Executa o bloco de código #3.
+        break;
+        // Para aqui
+        default:
+        // Se tudo falhar...
+        // Executa o bloco de código #4.
+        break;
+        // Para aqui
+    }
 
 */
