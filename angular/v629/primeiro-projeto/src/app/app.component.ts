960ee4891594387a6ecbf7d45b83820517eb9dcb
sergiodevsq <sergio@sq.dev.br>
2021-11-11 16:00:05 -0300

    Testes angular

---
+++ angular/v629/primeiro-projeto/src/app/app.component.ts
@@ -0,0 +1,17 @@
+declare var $: any;
+
+import { Component, OnInit } from '@angular/core';
+
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(){}
+
+  title = 'Olá mundo';
+}
---
+++ angular/v629/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+        <h1> Meu primeiro componente </h1>
+  `
+})
+export class MeuPrimeiroComponent{}
