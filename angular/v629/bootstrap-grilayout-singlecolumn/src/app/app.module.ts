69f98a0621ba20143693c329d489c62e46d661cb
sergiodevsq <sergio@sq.dev.br>
2021-11-13 15:31:51 -0300

    Atualizar app angular formulário

--- angular/v629/bootstrap-grilayout-singlecolumn/src/app/app.module.ts
+++ angular/v629/bootstrap-grilayout-singlecolumn/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './header/main-header/main-header.component';
+import { MainContentComponent } from './content/main-content/main-content.component';
+import { MainFooterComponent } from './footer/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule
